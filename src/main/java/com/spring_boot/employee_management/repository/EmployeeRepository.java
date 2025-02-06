4e93dced35cd8fd391fa91b08d5a37c983b0b630
Oybek <oybek@marvrus.com>
2025-02-06 23:02:23 +0900

    added jwt authentication and user entity and service

--- src/main/java/com/spring_boot/employee_management/repository/EmployeeRepository.java
+++ src/main/java/com/spring_boot/employee_management/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.spring_boot.employee_management.repository;
 
 import com.spring_boot.employee_management.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
